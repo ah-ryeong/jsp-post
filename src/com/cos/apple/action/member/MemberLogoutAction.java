06905ec091f9f3eff0da7fb73ac1161e38a1a395
unknown <aryeong22@hanail.net>
2020-06-19 11:17:45 +0900

    9. 로그아웃 만들기

--- src/com/cos/apple/action/member/MemberLogoutAction.java
+++ src/com/cos/apple/action/member/MemberLogoutAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cos.apple.action.Action;
 
@@ -13,7 +14,10 @@ public class MemberLogoutAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession session = request.getSession();
+		session.invalidate();
 		
+		response.sendRedirect("index.jsp");
 	}
 
 }
